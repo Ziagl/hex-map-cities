d55b753d4197a5bfc2b62facbd76bd70c5d11898
Ziagl <office@ziegelwanger-edv.at>
2025-03-21 16:44:15 +0100

    version 0.3.5

--- HexMapCities/Models/CityBase.cs
+++ HexMapCities/Models/CityBase.cs
@@ -1,21 +1,32 @@
-﻿using com.hexagonsimulations.Geometry.Hex.Interfaces;
+﻿using com.hexagonsimulations.HexMapBase.Interfaces;
 using com.hexagonsimulations.HexMapBase.Models;
 
 namespace com.hexagonsimulations.HexMapCities.Models;
 
-public class CityBase : GameEntity
+public class CityBase : ICombatEntity
 {
-    // base
-    public string Name = string.Empty;
+    // base entity
+    public int Id { get; set; }
+    public int Player { get; set; }
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    // base city
+    public string Name { get; set; } = string.Empty;
+    // combat
+    public int WeaponType { get; set; } // type of weapon/combat of this unit (infantry, cavalry, ...)
+    public int Attack { get; set; } // attack points (damage in fight)
+    public int RangedAttack { get; set; } // ranged attack points (damage of airstrike)
+    public int Defense { get; set; } // defence points (how much damage is reduced)
+    public int Range { get; set; } // attack range (how far can this unit attack)
     // position
-    public CubeCoordinates Position;
-    public List<CubeCoordinates> Tiles = new();
+    public CubeCoordinates Position { get; set; }
+    public List<CubeCoordinates> Tiles { get; set; } = new();
     // UI
-    public Point PositionPixel = new();      // absolute position in pixels
-    public List<Point> TilesPixel = new();   // absolute positions in pixels
-    public List<Line> Borders = new();       // border lines in absolute pixels
+    public Point PositionPixel { get; set; } = new();      // absolute position in pixels
+    public List<Point> TilesPixel { get; set; } = new();   // absolute positions in pixels
+    public List<Line> Borders { get; set; } = new();       // border lines in absolute pixels
     // buildings
-    public List<BuildingBase> Buildings = new();
+    public List<BuildingBase> Buildings { get; set; } = new();
     // properties
-    public Dictionary<string, object> Properties = new();
+    public Dictionary<string, object> Properties { get; set; } = new();
 }
