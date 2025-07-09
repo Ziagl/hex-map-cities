ae18f4c267268faf59c13173815bef6bf556bede
Ziagl <office@ziegelwanger-edv.at>
2025-07-09 19:16:00 +0200

    version 0.3.18

--- HexMapCities/Models/BuildingBase.cs
+++ HexMapCities/Models/BuildingBase.cs
@@ -5,7 +5,7 @@ namespace com.hexagonsimulations.HexMapCities.Models;
 public record BuildingBase : BuildingType
 {
     // base
-    
+    public bool IsActive = true; // is this building active?
     // position
     public CubeCoordinates Position; // its position on the map
 }
--- HexMapCities/Models/BuildingType.cs
+++ HexMapCities/Models/BuildingType.cs
@@ -13,9 +13,9 @@ public record BuildingType
     // economy
     public List<Tuple<int, int>> Goods_Production = new(); // goods produced by this building (goods id, amount)
     public List<Tuple<int, int>> Goods_Cost = new(); // goods needed to produce this building (goods id, amount)
-    public int Food; // food produced by this building
-    public int Production; // production of this building
-    public int Gold; // gold produced by this building
+    public int Food; // needed by this building
+    public int Production; // needed by this building
+    public int Gold; // upkeep for this building
     public int Science; // science produced by this building
     public int Citizens; // number of citizens this building habitats
     public int ProductionCost; // amount of production needed to build this building
